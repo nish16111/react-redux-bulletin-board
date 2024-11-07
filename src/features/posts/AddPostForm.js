96604563dea2bbd3ae2063fa4d7a08499dc9e79e
Nishant Sirwani <nishantsirwani@gmail.com>
2024-11-07 18:10:02 +0530

    Setting up users for the posts - ongoing

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,20 +1,27 @@
 import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { postAdded } from "./postsSlice";
-import { nanoid } from "@reduxjs/toolkit";
-import { useDispatch } from "react-redux";
+import { selectAllUsers } from "../users/usersSlice";
 
 const AddPostForm = () => {
+    const dispatch = useDispatch()
+
     const [title, setTitle] = useState('')
     const [content, setContent] = useState('')
-    const dispatch = useDispatch()
+    const [userId, setUserId] = useState('')
+
+    const users = useSelector(selectAllUsers)
 
     const onTitleChanged = (e) => setTitle(e.target.value);
     const onContentChanged = (e) => setContent(e.target.value);
+    const onAuthorChanged = (e) => setUserId(e.target.value);
+
+    console.log("UserId is: ", userId)
 
     const onSavePostClicked = () => {
 
         if(title && content){
-            dispatch(postAdded(title, content))
+            dispatch(postAdded(title, content, userId))
             setTitle('')
             setContent('')
             console.log("Data Saved..")
@@ -22,6 +29,14 @@ const AddPostForm = () => {
             console.log("Error Saving data")
         }
     }
+
+    const usersOption = users.map((users) => {
+        return(
+            <option key={users.id} value={users.id}>
+                {users.name}
+            </option>
+        )
+    })
 
     return(
         <section>
@@ -35,6 +50,15 @@ const AddPostForm = () => {
                     value={title}
                     onChange={onTitleChanged}
                 />
+                <label htmlFor="postAuthor">Authors:</label>
+                <select
+                    id="postAuthor"
+                    value={userId}
+                    onChange={onAuthorChanged}
+                >
+                    <option value=""></option>
+                    {usersOption}
+                </select>
                 <label htmlFor="postContent">Post Content:</label>
                 <textarea
                     type="text" 
