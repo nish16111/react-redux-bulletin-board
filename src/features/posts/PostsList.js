1142da02637cc470d2e46b39d12575b4e5966d94
Nishant Sirwani <nishantsirwani@gmail.com>
2024-11-08 12:28:16 +0530

    Added TimeAgo component

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux';
 import { selectAllPosts } from './postsSlice';
 import PostAuthor from './PostAuthor';
+import TimeAgo from './TimeAgo';
 
 const PostsList = () => {
 
@@ -18,6 +19,7 @@ const PostsList = () => {
                 <p>{posts.content.substring(0, 100)}</p>
                 <p className='postCredit'>
                     <PostAuthor userId={posts.userId}/>
+                    <TimeAgo timestamp={posts.date}/>
                 </p>
             </article>
         )
