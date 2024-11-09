57b6bb97add388fecd519113c8fbbe6fe129eb3e
Nishant Sirwani <nishantsirwani@gmail.com>
2024-11-09 11:56:37 +0530

    async thunks logic in redux implementation ongoing

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,5 +1,13 @@
-import { createSlice, nanoid } from "@reduxjs/toolkit";
+import { createSlice, nanoid, createAsyncThunk } from "@reduxjs/toolkit";
 import { sub } from 'date-fns';
+import axios from "axios";
+
+const POSTS_URL = 'https://jsonplaceholder.typicode.com/posts';
+
+export const fetchPosts = createAsyncThunk( 'posts/fetchPosts', async() => {
+    const response = await axios.get(POSTS_URL)
+    return response.data
+} )
 
 const initialState = {
     posts: [],
@@ -40,6 +48,36 @@ const postsSlice = createSlice({
             if(existingPost) {
                 existingPost.reactions[reaction]++
             }
+        },
+        extraReducers(builder) {
+            builder
+                .addCase(fetchPosts.pending, (state, action) => {
+                    state.status = 'loading...'
+                })
+
+                .addCase(fetchPosts.fulfilled, (state, action) => {
+                    state.status = 'succeeded'
+
+                    // Updated the state with posts and add reactions
+                    let min = 1;
+                    const loadedPosts = action.payload.map((post) => {
+                        post.date = sub(new Date(), {minutes: min++}).toISOString();
+                        post.reactions = {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            coffee: 0
+                        }
+                    })
+
+                    state.posts = state.posts.concat(loadedPosts);
+                })
+                
+                .addCase(fetchPosts.rejected, (state, action) => {
+                    state.status = 'failed';
+                    state.error = action.error.message;
+                })
         }
     }
 })
@@ -49,3 +87,22 @@ export const selectAllPosts = (state) => state.posts.posts;
 export const { postAdded, reactionAdded } = postsSlice.actions;
 
 export const postsReducer =  postsSlice.reducer;
+
+
+/*
+Async functions with Promises allow Redux thunks to handle async operations (like fetching data) 
+smoothly without blocking the rest of the code execution. By combining Promises with async/await, 
+async thunks help simplify managing and organizing async code within Redux.
+*/
+
+/*
+What is a Promise in the Context of async Functions and Thunks?
+A Promise is an object that represents an operation (like an API call) that may take some time to 
+complete. 
+
+It has three possible states:
+
+Pending: The operation is ongoing, and we don't yet know the result.
+Fulfilled: The operation completed successfully, and the result is available.
+Rejected: The operation failed, and an error message is available.
+ */
