8b4c456cb9cc4b2c1829bb9e5dbae1cc1239777b
Nishant Sirwani <nishantsirwani@gmail.com>
2024-11-11 17:55:23 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}/>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<PostsList />} />
 
-      <Route index element={<PostsList />} />
-
-      <Route path="post">
-        <Route index element={<AddPostForm />} />
-        <Route path=":postId" element={<SinglePostPage />} /> 
+        <Route path="post">
+          <Route index element={<AddPostForm />} />
+          <Route path=":postId" element={<SinglePostPage />} />
+        </Route>
       </Route>
     </Routes>
   );
