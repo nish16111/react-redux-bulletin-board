6f503f40af759dab8508e18e190bfde91566c951
Nishant Sirwani <nishantsirwani@gmail.com>
2024-11-12 11:43:49 +0530

    implementing EditPost feature

---
+++ src/features/posts/EditPostForm.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useDispatch } from 'react-redux'
+
+const EditPostForm = () => {
+
+    const dispatch = useDispatch()
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default EditPostForm
--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import PostAuthor from './PostAuthor';
 import ReactionButtons from './ReactionButtons';
 import TimeAgo from './TimeAgo';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 const SinglePostPage = () => {
 
@@ -26,6 +26,7 @@ const SinglePostPage = () => {
       <h2>{post.title}</h2>
       <p>{post.body}</p>
       <p className='postCredit'>
+        <Link to={`/post/edit/${post.id}`}>Edit Post</Link>
         <PostAuthor userId={post.userId}/>
         <TimeAgo timestamp={post.date}/>
       </p>
